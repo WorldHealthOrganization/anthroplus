ba12bcd221c320de172f27445d0fe5a5b833364a
Dirk Schumacher <mail@dirk-schumacher.net>
2021-11-22 17:43:15 +0100

    Update test data
    Remove further columns to make the test file smaller, as it
    is shipped with every package.

--- data-raw/testdata.R
+++ data-raw/testdata.R
@@ -1,8 +1,10 @@
 test_dataset_who2007 <- read.csv("data-raw/survey_who2007_z.csv")
 test_dataset_who2007$dob <- NULL
 test_dataset_who2007$dov <- NULL
+test_dataset_who2007$region <- NULL
+test_dataset_who2007$age.mo <- NULL
 saveRDS(
   test_dataset_who2007,
-  "inst/testdata/test_dataset_who2007.rds",
+  "tests/testthat/test_dataset_who2007.rds",
   version = 3
 )
